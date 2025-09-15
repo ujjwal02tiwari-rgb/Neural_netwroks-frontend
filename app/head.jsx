b5fec646bae51866c5e6ed4a8ee6d2e15c72688b
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-15 11:25:15 +0530

    Update head.jsx

--- app/head.jsx
+++ app/head.jsx
@@ -1,14 +1,12 @@
-
 export default function Head() {
   return (
-    
-      <meta name="api-base" content={process.env.NEXT_PUBLIC_API_BASE_
-        URL} />
-
-    
-    
+    <>
+      <meta name="api-base" content={process.env.NEXT_PUBLIC_API_BASE_URL} />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.__API_BASE__ = "${process.env.NEXT_PUBLIC_API_BASE_URL}";`,
+        }}
+      />
     </>
-    
   );
 }
-
